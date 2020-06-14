a076f0e519df0e62f6998a5311f19d45adc0a135
iamchets <iamchets@yahoo.com>
2020-06-14 14:49:57 +0200

    Messaged up conditionals in modal

--- client/src/components/generic/Widget.jsx
+++ client/src/components/generic/Widget.jsx
@@ -4,8 +4,17 @@ import { Ellipsis } from "react-awesome-spinners";
 import * as Styled from "../styles/Widget";
 
 const Widget = ({ children, size, loading, error, title, link = "more", href, count = 0, message = "" }) => {
-    if (count <= 0) {
-    }
+    const render = () => {
+        if (loading) {
+            return <Ellipsis />;
+        } else if (error) {
+            return <Styled.Widget.Item danger>Something went wrong...</Styled.Widget.Item>;
+        } else if (count <= 0) {
+            return <Styled.Widget.Item info>{message}</Styled.Widget.Item>;
+        } else {
+            return children;
+        }
+    };
 
     return (
         <Styled.Widget size={size}>
@@ -15,9 +24,7 @@ const Widget = ({ children, size, loading, error, title, link = "more", href, co
                     {link} <FaLongArrowAltRight className="ml-2 mt-02" />
                 </Styled.Widget.Link>
             </Styled.Widget.Wrapper>
-            {!error && loading ? <Ellipsis /> : children}
-            {!error && count <= 0 && <Styled.Widget.Item info>{message}</Styled.Widget.Item>}
-            {error && !loading && <Styled.Widget.Item danger>Something went wrong...</Styled.Widget.Item>}
+            {render()}
         </Styled.Widget>
     );
 };
