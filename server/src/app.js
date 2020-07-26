d6fc2ccc6e20144a7ccc8415d65e5b2207475eda
Donny Roufs <iamchets@yahoo.com>
2020-07-26 11:51:07 +0200

    database has validation now when connecting. cant find a native solution.

--- server/src/app.js
+++ server/src/app.js
@@ -2,17 +2,19 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import apiRoutes from "./api/routes/index";
+import { validateConnection } from "./db";
 import "./utils/datePrototypes";
 
 const app = express();
 
 (async () => {
-    app.use(cors());
-    app.use(express.json());
+  app.use(cors());
+  app.use(express.json());
 
-    app.use("/api", apiRoutes);
+  app.use("/api", apiRoutes);
 
-    app.listen(process.env.PORT, () => {
-        console.log(`App listening on http://localhost:${process.env.PORT}`);
-    });
+  await validateConnection();
+  app.listen(process.env.PORT, () => {
+    console.log(`App listening on http://localhost:${process.env.PORT}`);
+  });
 })();
--- server/src/db.js
+++ server/src/db.js
@@ -2,4 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
 
+export const validateConnection = async () => {
+  try {
+    await client.todo.findMany();
+    console.log("Database connected..");
+  } catch (err) {
+    throw err;
+  }
+};
+
 export default client;
